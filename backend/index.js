6a76e6b84e1dc367db84a7e67018e9a0383b2c47
Sreeshil C K <sreeshilck777@gmail.com>
2024-06-29 17:06:33 +0530

    Fix cors issue

--- backend/index.js
+++ backend/index.js
@@ -1,39 +1,37 @@
-const express = require ("express");
-const cors = require ("cors")
+const express = require("express");
+const cors = require("cors")
 const mongoose = require("mongoose")
 const Routes = require('./Routes/Route')
 const cookieParser = require("cookie-parser")
 const app = express();
 require("dotenv").config();
 
-
-
 app.listen(4000, () => {
     console.log("server is running on port 4000");
 });
 
-mongoose.connect("mongodb://localhost:27017/webapp", {
+mongoose.connect("mongodb://0.0.0.0:27017/webapp", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => {
     console.log("DB Connection Successful");
 }).catch((err) => {
-    console.log(err.message,"DB Connection Failed");
-}) 
+    console.log(err.message, "DB Connection Failed");
+})
 
 app.use(
     cors({
-        origin: ["http://localhost:3001"],
-        method: ["GET", "POST"],
-        credentials : true,
+        origin: ["http://localhost:3000"],
+        method: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true,
     })
 );
 
-app.use((req,res,next)=>{
-    res.set('Cache-Control','no-store')
+app.use((req, res, next) => {
+    res.set('Cache-Control', 'no-store')
     next()
-  })
- 
+})
+
 app.use(cookieParser());
 app.use(express.json());
 app.use("/", Routes)
